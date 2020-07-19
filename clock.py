c2f0b97e649d92bb90d976c85c2808785276c4f0
Chau Ngo <chauminhngo330@gmail.com>
2020-07-20 01:54:39 +0700

    make it better

--- clock.py
+++ clock.py
@@ -1,10 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
+import time
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
 def timed_job():
     subprocess.Popen(['python bot.py'],shell=True)
+    time.sleep(10)
 
 sched.start()
