0cbd01ab7b8b6c6052d8cac3f3c09e35715354d4
Chau Ngo <chauminhngo330@gmail.com>
2020-07-19 13:22:23 +0700

    make it better

--- clock.py
+++ clock.py
@@ -5,6 +5,6 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
 def timed_job():
-    os.system('cmd /c "python bot.py"')
+    os.system('python bot.py')
 
 sched.start()
