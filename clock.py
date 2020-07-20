3c0ba5f44b6cd05ef97afd93e9be315eade3745b
Chau Ngo <chauminhngo330@gmail.com>
2020-07-20 19:00:00 +0700

    make it better

--- clock.py
+++ clock.py
@@ -7,20 +7,8 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
 def timed_job():
-    proc = subprocess.Popen(['python TRXUSDT.py'],shell=True)
-    try:
-        proc.wait(timeout=0.3)
-    except subprocess.TimeoutExpired:
-        proc.terminate()
-    proc1 = subprocess.Popen(['python BNBUSDT.py'],shell=True)
-    try:
-        proc1.wait(timeout=0.3)
-    except subprocess.TimeoutExpired:
-        proc1.terminate()
-    proc2 = subprocess.Popen(['python BATUSDT.py'],shell=True)
-    try:
-        proc2.wait(timeout=0.3)
-    except subprocess.TimeoutExpired:
-        proc2.terminate()
+    subprocess.Popen(['python TRXUSDT.py'],shell=True)
+    subprocess.Popen(['python BNBUSDT.py'],shell=True)
+    subprocess.Popen(['python BATUSDT.py'],shell=True)
 
 sched.start()
