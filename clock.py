babad4179cda5e009537095bdac0db2869c0c370
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 20:13:41 +0700

    make it better

--- clock.py
+++ clock.py
@@ -2,18 +2,29 @@ import bot1
 import bot2
 import bot3
 from apscheduler.schedulers.blocking import BlockingScheduler
-import subprocess
+import threading
 import time
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59')
-def timed_job():
-    p = subprocess.Popen(['python bot1.py'])
-    p.terminate()
-    q = subprocess.Popen(['python bot2.py'])
-    q.terminate()
-    r = subprocess.Popen(['python bot3.py'])
-    r.terminate()
+    def trx():
+        bot1.main()
+    def bat():
+        bot2.main()
+    def bnb():
+        bot3.main()
 
+    try:
+        t1 = threading.Thread(target=bnb)
+        t2 = threading.Thread(target=bat)
+        t3 = threading.Thread(target=trx)
+        t1.start()
+        t2.start()
+        t3.start()
+        t1.join()
+        t2.join()
+        t3.join()
+    except:
+        print ("error")
 sched.start()
