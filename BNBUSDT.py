85bcb2a9820f2dc30a0ffee5ccda8d992248c313
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 08:59:52 +0700

    make it better

--- BNBUSDT.py
+++ BNBUSDT.py
@@ -211,4 +211,3 @@ def main():
     interval = '15m'
     step1 = start(symbol, quote, base, step_size, leverage, interval)
 
-main()
--- clock.py
+++ clock.py
@@ -1,6 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 import time
+import trxusdt
+import bnbusdt
+import batusdt
 
 sched = BlockingScheduler()
 
@@ -8,13 +11,12 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
  
 def timed_job():
-    p = subprocess.Popen(['TRXUSDT.py'], stdout=subprocess.PIPE, shell=False)
-    p.terminate()
+    p = trxusdt.main()
 
-    q = subprocess.Popen(['BNBUSDT.py'], stdout=subprocess.PIPE, shell=False)
+    q = bnbusdt.main()
     q.terminate()
     
-    r = subprocess.Popen(['BATUSDT.py'], stdout=subprocess.PIPE, shell=False)
+    r = batusdt.main()
     r.terminate()
     
 sched.start()
