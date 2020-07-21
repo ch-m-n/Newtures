c74a14ef66ea947b9d70cae4b11444724b5f1b3a
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 20:21:55 +0700

    make it better

--- clock.py
+++ clock.py
@@ -10,23 +10,24 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59')
 
-    def trx():
+def trx():
         bot1.main()
-    def bat():
+def bat():
         bot2.main()
-    def bnb():
-        bot3.main()
+def bnb():
+    bot3.main()
 
-    try:
-        t1 = threading.Thread(target=bnb)
-        t2 = threading.Thread(target=bat)
-        t3 = threading.Thread(target=trx)
-        t1.start()
-        t2.start()
-        t3.start()
-        t1.join()
-        t2.join()
-        t3.join()
-    except:
-        print ("error")
+try:
+    t1 = threading.Thread(target=bnb)
+    t2 = threading.Thread(target=bat)
+    t3 = threading.Thread(target=trx)
+    t1.start()
+    t2.start()
+    t3.start()
+    t1.join()
+    t2.join()
+    t3.join()
+except:
+    print ("error")
+    
 sched.start()
