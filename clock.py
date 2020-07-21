f8d3c331bab5bdc25394855f862f0530a9630bb1
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 20:45:55 +0700

    make it better

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ import time
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
 
 def trx():
     bot1.main()
