0b7a8437e79e0cf3cbbcb7b9f9edccf0e225fdde
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 21:03:01 +0700

    make it better

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ import time
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59')
 
 def trx():
     bot1.main()
@@ -20,7 +20,7 @@ try:
     t2 = threading.Thread(target=trx)
     t1.start()
     t2.start()
-
+    
 except:
     print ("error")
 
