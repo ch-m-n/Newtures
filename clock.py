267b04f58fe276a9b0f61cb85ed3a1238e172a48
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 07:56:03 +0700

    make it better

--- clock.py
+++ clock.py
@@ -8,13 +8,13 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
  
 def timed_job():
-    p = subprocess.Popen(['python TRXUSDT.py'],shell=True)
+    p = subprocess.Popen(['python TRXUSDT.py'], stdout=subprocess.PIPE, shell=False)
     p.terminate()
 
-    q = subprocess.Popen(['python BNBUSDT.py'],shell=True)
+    q = subprocess.Popen(['python BNBUSDT.py'], stdout=subprocess.PIPE, shell=False)
     q.terminate()
     
-    r = subprocess.Popen(['python BATUSDT.py'],shell=True)
+    r = subprocess.Popen(['python BATUSDT.py'], stdout=subprocess.PIPE, shell=False)
     r.terminate()
     
 sched.start()
