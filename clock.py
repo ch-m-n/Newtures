d0f8924263c37419c5961e05dd60fd34dc17407d
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 09:53:55 +0700

    make it better

--- clock.py
+++ clock.py
@@ -10,11 +10,13 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
  
-def timed_job():
+def timed_job1():
     p = bot1.main()
 
+def timed_job2():
     q = bot2.main()
-    
+
+def timed_job3():
     r = bot3.main()
     
 sched.start()
