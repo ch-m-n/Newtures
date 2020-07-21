1f3062007873d0a832b89fc01289e4b3f0879cf9
Chau Ngo <chauminhngo330@gmail.com>
2020-07-21 11:00:38 +0700

    make it better

--- clock.py
+++ clock.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     while True:
         if datetime.datetime.now().minute % 15 == 0: 
             bot1.main()
-            bot2,main()
+            bot2.main()
             bot3.main()
         time.sleep(1)
