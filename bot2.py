90a03c4a3dceb87d2c093c65155b6336b60ec4b7
Chau Ngo <chauminhngo330@gmail.com>
2020-07-22 21:46:10 +0700

    make it better

--- bot2.py
+++ bot2.py
@@ -71,11 +71,11 @@ class start:
         adx = float(adx[499])
 
         #TRIX EMA
-        trix = TRIX(df['close'], timeperiod=10)
+        trix = TRIX(df['close'], timeperiod=20)
         tx = float(trix[499])*100
 
         red = EMA(df['close'], timeperiod=7)
-        trema = TRIX(red, timeperiod=10)
+        trema = TRIX(red, timeperiod=20)
         tema = float(trema[499])*100
 
         #STOCHRSI
