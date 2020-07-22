83e8618302039c662e7be280ce4e41654a847850
Chau Ngo <chauminhngo330@gmail.com>
2020-07-22 16:11:09 +0700

    make it better

--- clock.py
+++ clock.py
@@ -12,7 +12,7 @@ sleeptime = 60 - (t.second + t.microsecond/1000000.0)
 
 if __name__ == '__main__':
     while True:
-        if datetime.datetime.now().minute % 15 == 0:
+        if datetime.datetime.now().minute % 15 == 0 and datetime.datetime.now().second == 0:
             bot1.main()
             bot2.main()
             bot3.main()
