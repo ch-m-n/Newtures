ab3067aeef4c875d4be4446dd07e23dce28e1ef0
Chau Ngo <chauminhngo330@gmail.com>
2020-07-22 22:32:07 +0700

    make it better

--- clock.py
+++ clock.py
@@ -7,16 +7,16 @@ import time
 import datetime
 import sys
     
-t = datetime.datetime.now()
-sleeptime = 60 - (t.second + t.microsecond/1000000.0)
-
+def con():
+    print(datetime.datetime.now())
+    bot1.run_trx()
+    bot2.run_bat()
+    bot3.run_xlm()
+    bot4.run_vet()
+    
 if __name__ == '__main__':
     while True:
         if datetime.datetime.now().minute % 15 == 0:
-            print(datetime.datetime.now())
-            bot1.run_trx()
-            bot2.run_bat()
-            bot3.run_xlm()
-            bot4.run_vet()
+            con()
         time.sleep(60)
 
