0734a9cb3de782d0056279c1445662cb0d8f3fc0
Chau Ngo <chauminhngo330@gmail.com>
2020-07-22 21:19:56 +0700

    make it better

--- clock.py
+++ clock.py
@@ -2,20 +2,21 @@ import bot1
 import bot2
 import bot3
 import bot4
-from apscheduler.schedulers.blocking import BlockingScheduler
-import subprocess
+from threading import Thread
 import time
+import datetime
+import sys
+    
+t = datetime.datetime.now()
+sleeptime = 60 - (t.second + t.microsecond/1000000.0)
 
-def con():
-    bot1.run_trx()
-    bot2.run_bat()
-    bot3.run_xlm()
-    bot4.run_vet()
+if __name__ == '__main__':
+    while True:
+        if datetime.datetime.now().minute % 15 == 0:
+            print(datetime.datetime.now())
+            bot1.run_trx()
+            bot2.run_bat()
+            bot3.run_xlm()
+            bot4.run_vet()
+        time.sleep(60)
 
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15')
-def timed_job():
-    con()
-
-sched.start()
