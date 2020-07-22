6e0194b28de87923ed3332dd47ed5e5a3bee3b13
Chau Ngo <chauminhngo330@gmail.com>
2020-07-22 12:03:57 +0700

    make it better

--- clock.py
+++ clock.py
@@ -8,9 +8,7 @@ import datetime
 if __name__ == '__main__':
     while True:
         if datetime.datetime.now().minute % 15 == 0 and datetime.datetime.now().second == 0:
-            t1 = Thread(target=bot1.main())
-            t2 = Thread(target=bot2.main())
-            t3 = Thread(target=bot3.main())
-            t1.start()
-            t2.start()
-            t3.start()
+            bot1.main()
+            bot2.main()
+            bot3.main()
+
