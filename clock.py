911aa9942eac77428afa0812b1ee641119d7e1cd
Chau Ngo <chauminhngo330@gmail.com>
2020-07-22 23:32:45 +0700

    make it better

--- clock.py
+++ clock.py
@@ -2,18 +2,25 @@ import bot1
 import bot2
 import bot3
 import bot4
-from threading import Thread
+import threading
 import time
 import datetime
-import sys
     
 def con():
     print(datetime.datetime.now())
-    bot1.run_trx()
-    bot2.run_bat()
-    bot3.run_xlm()
-    bot4.run_vet()
-    
+    t1 = threading.Thread(target=bot1.run_trx())
+    t1.start()
+    t1.join()
+    t2 = threading.Thread(target=bot2.run_bat())
+    t2.start()
+    t2.join()
+    t3 = threading.Thread(target=bot3.run_xlm())
+    t3.start()
+    t3.join()
+    t3 = threading.Thread(target=bot4.run_vet())
+    t3.start()
+    t3.join()
+
 if __name__ == '__main__':
     while True:
         if datetime.datetime.now().minute % 15 == 0:
