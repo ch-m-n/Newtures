3f86c625cd442a22f7c7e28cde4b28c3601d215b
Chau Ngo <chauminhngo330@gmail.com>
2020-07-24 20:45:01 +0700

    make it better

--- clock.py
+++ clock.py
@@ -22,9 +22,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15', max_instances=5)
 def timed_job():
-    try:
-        con()
-    except:
+    con()
         
 
 sched.start()
