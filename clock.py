82c5efe1185962df14ecbd59062f62d7c6182927
chaungo <chaungo@localhost.localdomain>
2020-07-27 21:24:26 +0700

    make it better

--- clock.py
+++ clock.py
@@ -43,7 +43,7 @@ def con():
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15', max_instances=5)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15', max_instances=15)
 def timed_job():
     con()
         
