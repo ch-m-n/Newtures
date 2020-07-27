9101c47368bda60bb59d8703ccda240f99e70384
chaungo <chaungo@localhost.localdomain>
2020-07-27 18:01:04 +0700

    make it better

--- clock.py
+++ clock.py
@@ -8,28 +8,28 @@ import list
     
 def con():
     print(datetime.datetime.now())
-    t1 = threading.Thread(target=bot.run_trx(pair=list.trx['symbol'], 
+    t1 = threading.Thread(target=bot.run(pair=list.trx['symbol'], 
                                                 q=list.trx['quote'], 
                                                 b=list.trx['base'], 
                                                 step=list.trx['step'],
                                                 levr=list.trx['leverage'],
                                                 t=list.trx['interval'],
                                                 r=list.trx['roundQuant']))
-    t2 = threading.Thread(target=bot.run_bat(pair=list.bat['symbol'], 
+    t2 = threading.Thread(target=bot.run(pair=list.bat['symbol'], 
                                                 q=list.bat['quote'], 
                                                 b=list.bat['base'], 
                                                 step=list.bat['step'],
                                                 levr=list.bat['leverage'],
                                                 t=list.bat['interval'],
                                                 r=list.bat['roundQuant']))
-    t3 = threading.Thread(target=bot.run_xlm(pair=list.xlm['symbol'], 
+    t3 = threading.Thread(target=bot.run(pair=list.xlm['symbol'], 
                                                 q=list.xlm['quote'], 
                                                 b=list.xlm['base'], 
                                                 step=list.xlm['step'],
                                                 levr=list.xlm['leverage'],
                                                 t=list.xlm['interval'],
                                                 r=list.xlm['roundQuant']))
-    t4 = threading.Thread(target=bot.run_iota(pair=list.iota['symbol'], 
+    t4 = threading.Thread(target=bot.run(pair=list.iota['symbol'], 
                                                 q=list.iota['quote'], 
                                                 b=list.iota['base'], 
                                                 step=list.iota['step'],
