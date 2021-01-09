23554755029673e823e03d58d4ee993a0eabb57c
chaungo <chaungo@localhost.localdomain>
2021-01-09 16:52:42 +0700

    Reconstructed main branch

--- clock.py
+++ clock.py
@@ -3,33 +3,15 @@ import threading
 import time
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
-import list
-
-trx = list.trx
-xlm = list.xlm
-doge = list.doge
 
 def con():
-    print(datetime.datetime.now())
-<<<<<<< HEAD
-    t1 = threading.Thread(target=strategy.run(**trx))
-    t2 = threading.Thread(target=strategy.run(**bat))
-    t3 = threading.Thread(target=strategy.run(**xlm))
-    t4 = threading.Thread(target=strategy.run(**iota))
-=======
-    t1 = threading.Thread(target=bot.run(**trx))
-    t2 = threading.Thread(target=bot.run(**xlm))
-    t3 = threading.Thread(target=bot.run(**doge))
->>>>>>> 09f82f4a20f1e9c65073a5ff8d97d8ad926d556b
+    t1 = threading.Thread(target=strategy.run())
     t1.start()
-    t2.start()
-    t3.start()
-
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour = '0-23', minute = '0-59/15', max_instances=15, misfire_grace_time=3600)
 def timed_job():
     con()
-        
+
 sched.start()
--- strategy.py
+++ strategy.py
@@ -2,8 +2,6 @@ import pandas as pd
 import Binance
 import math
 from talib import EMA, ATR, RSI, TRIX, MA
-import tulipy as ti
-import numpy as np 
 
 def floatPrecision(f, n):
     n = int(math.log10(1 / float(n)))
@@ -11,24 +9,40 @@ def floatPrecision(f, n):
     f = "{:0.0{}f}".format(float(f), n)
     return str(int(f)) if int(n) == 0 else f
 
+def decimalize(n):
+    if n == 1:
+        t = 0.1
+    elif n == 0: 
+        t = 1
+    else:
+        q = '{0:.' + '{}'.format(n-1) +'f}'
+        t = str.format(q, 0) + '1'
+    return float(t)
+
 class start:
-    def __init__(self, symbol, quote, base, step_size, leverage, interval, roundQuant, position):
+    def __init__(self, symbol, quote, step_size, leverage, interval, roundQuant):
         self.symbol = symbol
         self.quote = quote
-        self.base = base
+        self.base = symbol[:-4]
         self.step_size = step_size
         self.leverage = leverage
         self.interval = interval
         self.roundQuant = roundQuant
-        self.position = position
+        self.pos = self.position()
+        self.openPosition = float(Binance.client.futures_position_information()[self.pos]['positionAmt'])
+        self.entryPrice = float(Binance.client.futures_position_information()[self.pos]['entryPrice'])
         self.df = self.getData()
         self.changeLeverage = self.changeLeverage()
-        self.openPosition = float(Binance.client.futures_position_information()[self.position]['positionAmt'])
-        self.quoteBalance = float(Binance.client.futures_account_balance(asset=self.quote)['balance'])
-        self.baseBalance = float(Binance.client.futures_get_all_orders(symbol=self.symbol)[-1]['origQty'])
+        self.quoteBalance = float(Binance.client.futures_account_balance()[0]['balance'])
         self.Quant = self.checkQuant()
         self.fire = self.strategy()
-        
+
+    def position(self):
+        p = Binance.client.futures_position_information()
+        for i in p:
+            if i['symbol'] == self.symbol:
+                return p.index(i)
+
     def getData(self):
         candles = Binance.client.futures_klines(symbol=self.symbol, interval=self.interval)
 
@@ -48,15 +62,31 @@ class start:
     def changeLeverage(self):
         def change():
             change = Binance.client.futures_change_leverage(
-                symbol=self.symbol, 
+                symbol=self.symbol,
                 leverage=self.leverage)
         return change()
-
+    
     def checkQuant(self):
-        df = self.df    
-        canBuySell = (float(self.quoteBalance)/float(self.df['close'][499]))*0.05*self.leverage
+        df = self.df
+        canBuySell = (float(self.quoteBalance)/float(df['close'].iloc[-1]))*0.002*self.leverage
         BuySellQuant = floatPrecision(canBuySell, self.roundQuant)
         return BuySellQuant
+
+    def getData(self):
+        candles = Binance.client.futures_klines(symbol=self.symbol, interval=self.interval)
+
+        #Sorting candlestick using Pandas
+        df = pd.DataFrame(candles)
+        df = df.drop(range(6, 12), axis=1)
+
+        # put in dataframe and clean-up
+        col_names = ['time', 'open', 'high', 'low', 'close', 'volume']
+        df.columns = col_names
+        # transform values from strings to floats
+        for col in col_names:
+            df[col] = df[col].astype(float)
+
+        return df
 
     def strategy(self):
 
@@ -155,42 +185,70 @@ class start:
                 closePosition='true')
 
         """Set up your strategy here"""
-
         """Conditions for trading"""
 
         longCond = current > baseline and tx > tema and self.openPosition == 0
-        closeLongCond = tx < tema and self.openPosition > 0
+        closeLong = tx < tema and self.openPosition > 0
         shortCond = current < baseline and tx < tema and self.openPosition == 0
-        closeShortCond = tx > tema and self.openPosition < 0
+        closeShort = tx > tema and self.openPosition < 0
+        #############################################
 
-        while longCond == True:
-            placeBuyOrder()
-            print('BUY ORDER PLACED on', self.base)
-            break
-            
-        while closeLongCond == True:
-            closeBuyOrder()
-            print('Closed BUY ORDER on', self.base)
-            break
+        if self.quoteBalance > current:
+            while longCond == True:
+                try:
+                    clearOrders()
+                    placeBuyOrder()
+                    longStop()
+                    print('Long order opened on', self.symbol)
+                    break
+                except:
+                    break
 
-        while shortCond == True:
-            placeSellOrder()
-            print('SELL ORDER PLACED on', self.base)
-            break
+            while shortCond == True:
+                try:
+                    clearOrders()
+                    placeSellOrder()
+                    shortStop()
+                    print('Short order opened on', self.symbol)
+                    break
+                except:
+                    break
             
-        while closeShortCond == True:
-            closeSellOrder()
-            print('Closed SELL ORDER on', self.base)
-            break
-        
-def run(pair, q, b, step, levr, t, r, p):
-    symbol = pair
-    quote = q
-    base = b
-    step_size = step
-    leverage = levr
-    interval = t
-    roundQuant = r
-    position = p
-    step1 = start(symbol, quote, base, step_size, leverage, interval, roundQuant, position)
+            while closeLong == True:
+                try:
+                    closeBuyOrder()
+                    clearOrders()
+                    print('Long order closed on', self.symbol)
+                    break
+                except:
+                    break
+
+            while closeShort == True:
+                try:
+                    closeSellOrder()
+                    clearOrders()
+                    print('Short order closed on', self.symbol)
+                    break
+                except:
+                    break
+            
+def run():
+    symbol = []
+    quote = 'USDT'
+    step_size = []
+    leverage = 20
+    interval = '15m'
+    roundQuant = []
+
+    t = Binance.client.futures_exchange_info()
+
+    for i in t['symbols']:
+        if i['symbol'].endswith(quote):
+            symbol.append(i['symbol'])
+            step_size.append(decimalize(i['pricePrecision']))
+            roundQuant.append(decimalize(i['quantityPrecision']))
+
+
+    for a,b,c in zip(symbol, step_size, roundQuant):
+        start(a, quote, b, leverage, interval, c)
 
